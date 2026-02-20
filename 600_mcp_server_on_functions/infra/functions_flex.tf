41830c346766cbb6154643aa94f3b46758b3e087
Houssem Dellai <houssem.dellai@live.com>
2026-02-20 17:31:29 +0000

    work in progress

--- 600_mcp_server_on_functions/infra/functions_flex.tf
+++ 600_mcp_server_on_functions/infra/functions_flex.tf
@@ -60,21 +60,22 @@ resource "azurerm_role_assignment" "storage_roleassignment" {
   principal_id         = azapi_resource.function_app.identity[0].principal_id
 }
 
-# get the system keys for the function app
-data "azapi_resource_action" "function_app_system_keys" {
-  type        = "Microsoft.Web/sites/host@2024-04-01"
-  resource_id = "${azapi_resource.function_app.id}/host/default"
-  action      = "listkeys"
-  method      = "POST"
-
-  response_export_values = ["*"]
-}
-
 output "functions_hostname" {
   value = azapi_resource.function_app.output.properties.defaultHostName
 }
 
-output "function_app_system_keys_mcp_extension" {
-  value     = try(data.azapi_resource_action.function_app_system_keys.output.systemKeys.mcp_extension, "")
-  sensitive = true
-}
+# # get the system keys for the function app
+# # !!! the keys are only available afetr deploying the MCP Server functions
+# data "azapi_resource_action" "function_app_system_keys" {
+#   type        = "Microsoft.Web/sites/host@2024-04-01"
+#   resource_id = "${azapi_resource.function_app.id}/host/default"
+#   action      = "listkeys"
+#   method      = "POST"
+
+#   response_export_values = ["*"]
+# }
+
+# output "function_app_system_keys_mcp_extension" {
+#   value     = try(data.azapi_resource_action.function_app_system_keys.output.systemKeys.mcp_extension, "")
+#   sensitive = true
+# }
