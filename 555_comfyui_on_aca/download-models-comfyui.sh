19d2129b48eb0bdf1e477320fdb4a8f01f220978
Houssem Dellai <houssem.dellai@live.com>
2026-03-03 00:58:18 +0100

    work in progress

--- 555_comfyui_on_aca/download-models-comfyui.sh
+++ 555_comfyui_on_aca/download-models-comfyui.sh
@@ -70,16 +70,16 @@ wget -O models/loras/Qwen-Image-Lightning-4steps-V1.0.safetensors \
 # # Download Flux 2 Klein Text to Image components
 # ######################################################
 
-wget -P models/text_encoders/ https://huggingface.co/Comfy-Org/flux2-klein-9B/resolve/main/split_files/text_encoders/qwen_3_8b_fp8mixed.safetensors
+wget -P models/text_encoders/qwen_3_8b_fp8mixed.safetensors https://huggingface.co/Comfy-Org/flux2-klein-9B/resolve/main/split_files/text_encoders/qwen_3_8b_fp8mixed.safetensors
 
-wget -P models/vae/ https://huggingface.co/Comfy-Org/flux2-dev/resolve/main/split_files/vae/flux2-vae.safetensors
+wget -P models/vae/flux2-vae.safetensors https://huggingface.co/Comfy-Org/flux2-dev/resolve/main/split_files/vae/flux2-vae.safetensors
 
-wget -P models/diffusion_models/ https://huggingface.co/black-forest-labs/FLUX.2-klein-base-9b-fp8/resolve/main/flux-2-klein-base-9b-fp8.safetensors
+wget -P models/diffusion_models/flux-2-klein-base-9b-fp8.safetensors https://huggingface.co/black-forest-labs/FLUX.2-klein-base-9b-fp8/resolve/main/flux-2-klein-base-9b-fp8.safetensors
 
-wget -P models/diffusion_models/ https://huggingface.co/black-forest-labs/FLUX.2-klein-9b-fp8/resolve/main/flux-2-klein-9b-fp8.safetensors
+wget -P models/diffusion_models/flux-2-klein-9b-fp8.safetensors https://huggingface.co/black-forest-labs/FLUX.2-klein-9b-fp8/resolve/main/flux-2-klein-9b-fp8.safetensors
 
 # ########################################################
-# # Download 
+# # Download Video Capybara Video Edit components
 # ########################################################
 
 wget -P models/diffusion_models/capybara_v0.1.safetensors https://huggingface.co/Comfy-Org/HunyuanVideo_1.5_repackaged/resolve/main/split_files/diffusion_models/capybara_v0.1.safetensors
