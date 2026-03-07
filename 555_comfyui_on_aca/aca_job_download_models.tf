1f1bbf18fd600defc84f90232f30317a1d4a42e5
Houssem Dellai <houssem.dellai@live.com>
2026-03-07 01:11:15 +0100

    work in progress

--- 555_comfyui_on_aca/aca_job_download_models.tf
+++ 555_comfyui_on_aca/aca_job_download_models.tf
@@ -4,7 +4,7 @@ resource "azurerm_container_app_job" "aca_job_download_models" {
   resource_group_name          = azurerm_resource_group.rg.name
   container_app_environment_id = azurerm_container_app_environment.aca_environment.id
   workload_profile_name        = "Consumption"
-  replica_timeout_in_seconds   = 1200
+  replica_timeout_in_seconds   = 1800 # 30 minutes
   replica_retry_limit          = 10
 
   manual_trigger_config {
@@ -16,8 +16,8 @@ resource "azurerm_container_app_job" "aca_job_download_models" {
     container {
       name    = "download-models"
       image   = "ubuntu:24.04"
-      cpu     = 0.5
-      memory  = "1Gi"
+      cpu     = 1
+      memory  = "2Gi"
       command = ["/bin/bash"]
       args    = ["-c", "apt-get update && apt-get install -y wget && wget https://raw.githubusercontent.com/HoussemDellai/ai-course/refs/heads/main/555_comfyui_on_aca/download-models-comfyui.sh && chmod +x /download-models-comfyui.sh && /download-models-comfyui.sh"]
 
--- 555_comfyui_on_aca/download-models-comfyui.sh
+++ 555_comfyui_on_aca/download-models-comfyui.sh
@@ -7,19 +7,19 @@ cd /root/ComfyUI/
 ########################################################
 
 # 1. text encoder
-wget -O models/text_encoders/qwen_3_4b.safetensors \
+wget -P models/text_encoders/ \
   https://huggingface.co/Comfy-Org/z_image_turbo/resolve/main/split_files/text_encoders/qwen_3_4b.safetensors
 
 # 2. VAE
-wget -O models/vae/ae.safetensors \
+wget -P models/vae/ \
   https://huggingface.co/Comfy-Org/z_image_turbo/resolve/main/split_files/vae/ae.safetensors
 
 # 3. diffusion model
-wget -O models/diffusion_models/z_image_turbo_bf16.safetensors \
+wget -P models/diffusion_models/ \
   https://huggingface.co/Comfy-Org/z_image_turbo/resolve/main/split_files/diffusion_models/z_image_turbo_bf16.safetensors
 
 # 4. Lora
-wget -O models/loras/pixel_art_style_z_image_turbo.safetensors \
+wget -P models/loras/ \
   https://huggingface.co/tarn59/pixel_art_style_lora_z_image_turbo/resolve/main/pixel_art_style_z_image_turbo.safetensors
   
 ################################################
@@ -27,67 +27,76 @@ wget -O models/loras/pixel_art_style_z_image_turbo.safetensors \
 ################################################
 
 # 1. text encoder
-wget -O models/text_encoders/umt5_xxl_fp8_e4m3fn_scaled.safetensors \
+wget -P models/text_encoders/ \
   https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/text_encoders/umt5_xxl_fp8_e4m3fn_scaled.safetensors
 
 # 2. VAE
-wget -O models/vae/wan_2.1_vae.safetensors \
+wget -P models/vae/ \
   https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/vae/wan_2.1_vae.safetensors
 
 # 3. diffusion model (low noise)
-wget -O models/diffusion_models/wan2.2_t2v_low_noise_14B_fp8_scaled.safetensors \
+wget -P models/diffusion_models/ \
   https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_t2v_low_noise_14B_fp8_scaled.safetensors
 
 # 4. diffusion model (high noise)
-wget -O models/diffusion_models/wan2.2_t2v_high_noise_14B_fp8_scaled.safetensors \
+wget -P models/diffusion_models/ \
   https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/diffusion_models/wan2.2_t2v_high_noise_14B_fp8_scaled.safetensors
 
 # 5. Lora (high noise)
-wget -O models/loras/wan2.2_t2v_lightx2v_4steps_lora_v1.1_high_noise.safetensors \
+wget -P models/loras/ \
   https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/loras/wan2.2_t2v_lightx2v_4steps_lora_v1.1_high_noise.safetensors
 
 # 6. Lora (low noise) – filename inferred from URL
-wget -O models/loras/wan2.2_t2v_lightx2v_4steps_lora_v1.1_low_noise.safetensors \
+wget -P models/loras/ \
   https://huggingface.co/Comfy-Org/Wan_2.2_ComfyUI_Repackaged/resolve/main/split_files/loras/wan2.2_t2v_lightx2v_4steps_lora_v1.1_low_noise.safetensors
 
 ######################################################
 # Download QWEN Image 2512 Text to Image components
 ######################################################
 
-wget -O models/text_encoders/qwen_2.5_vl_7b_fp8_scaled.safetensors \
+wget -P models/text_encoders/ \
   https://huggingface.co/Comfy-Org/Qwen-Image_ComfyUI/resolve/main/split_files/text_encoders/qwen_2.5_vl_7b_fp8_scaled.safetensors
 
-wget -O models/vae/qwen_image_vae.safetensors \
+wget -P models/vae/ \
   https://huggingface.co/Comfy-Org/Qwen-Image_ComfyUI/resolve/main/split_files/vae/qwen_image_vae.safetensors
 
-wget -O models/diffusion_models/qwen_image_2512_fp8_e4m3fn.safetensors \
+wget -P models/diffusion_models/ \
   https://huggingface.co/Comfy-Org/Qwen-Image_ComfyUI/resolve/main/split_files/diffusion_models/qwen_image_2512_fp8_e4m3fn.safetensors
 
-wget -O models/loras/Qwen-Image-Lightning-4steps-V1.0.safetensors \
+wget -P models/loras/ \
   https://huggingface.co/lightx2v/Qwen-Image-Lightning/resolve/main/Qwen-Image-Lightning-4steps-V1.0.safetensors
 
 # ######################################################
 # # Download Flux 2 Klein Text to Image components
 # ######################################################
 
-wget -P models/text_encoders/qwen_3_8b_fp8mixed.safetensors https://huggingface.co/Comfy-Org/flux2-klein-9B/resolve/main/split_files/text_encoders/qwen_3_8b_fp8mixed.safetensors
+wget -P models/text_encoders/ \
+  https://huggingface.co/Comfy-Org/flux2-klein-9B/resolve/main/split_files/text_encoders/qwen_3_8b_fp8mixed.safetensors
 
-wget -P models/vae/flux2-vae.safetensors https://huggingface.co/Comfy-Org/flux2-dev/resolve/main/split_files/vae/flux2-vae.safetensors
+wget -P models/vae/ \
+  https://huggingface.co/Comfy-Org/flux2-dev/resolve/main/split_files/vae/flux2-vae.safetensors
 
-wget -P models/diffusion_models/flux-2-klein-base-9b-fp8.safetensors https://huggingface.co/black-forest-labs/FLUX.2-klein-base-9b-fp8/resolve/main/flux-2-klein-base-9b-fp8.safetensors
+wget -P models/diffusion_models/ \
+  https://huggingface.co/black-forest-labs/FLUX.2-klein-base-9b-fp8/resolve/main/flux-2-klein-base-9b-fp8.safetensors
 
-wget -P models/diffusion_models/flux-2-klein-9b-fp8.safetensors https://huggingface.co/black-forest-labs/FLUX.2-klein-9b-fp8/resolve/main/flux-2-klein-9b-fp8.safetensors
+wget -P models/diffusion_models/ \
+  https://huggingface.co/black-forest-labs/FLUX.2-klein-9b-fp8/resolve/main/flux-2-klein-9b-fp8.safetensors
 
 # ########################################################
 # # Download Video Capybara Video Edit components
 # ########################################################
 
-wget -P models/diffusion_models/capybara_v0.1.safetensors https://huggingface.co/Comfy-Org/HunyuanVideo_1.5_repackaged/resolve/main/split_files/diffusion_models/capybara_v0.1.safetensors
+wget -P models/diffusion_models/ \
+  https://huggingface.co/Comfy-Org/HunyuanVideo_1.5_repackaged/resolve/main/split_files/diffusion_models/capybara_v0.1.safetensors
 
-wget -P models/text_encoders/qwen_2.5_vl_7b.safetensors https://huggingface.co/Comfy-Org/HunyuanImage_2.1_ComfyUI/resolve/main/split_files/text_encoders/qwen_2.5_vl_7b.safetensors
+wget -P models/text_encoders/ \
+  https://huggingface.co/Comfy-Org/HunyuanImage_2.1_ComfyUI/resolve/main/split_files/text_encoders/qwen_2.5_vl_7b.safetensors
 
-wget -P models/text_encoders/byt5_small_glyphxl_fp16.safetensors https://huggingface.co/Comfy-Org/HunyuanVideo_1.5_repackaged/resolve/main/split_files/text_encoders/byt5_small_glyphxl_fp16.safetensors
+wget -P models/text_encoders/ \
+  https://huggingface.co/Comfy-Org/HunyuanVideo_1.5_repackaged/resolve/main/split_files/text_encoders/byt5_small_glyphxl_fp16.safetensors
 
-wget -P models/vae/hunyuanvideo15_vae_fp16.safetensors https://huggingface.co/Comfy-Org/HunyuanVideo_1.5_repackaged/resolve/main/split_files/vae/hunyuanvideo15_vae_fp16.safetensors
+wget -P models/vae/ \
+  https://huggingface.co/Comfy-Org/HunyuanVideo_1.5_repackaged/resolve/main/split_files/vae/hunyuanvideo15_vae_fp16.safetensors
 
-wget -P models/clip_vision/sigclip_vision_patch14_384.safetensors https://huggingface.co/Comfy-Org/sigclip_vision_384/resolve/main/sigclip_vision_patch14_384.safetensors
+wget -P models/clip_vision/ \
+  https://huggingface.co/Comfy-Org/sigclip_vision_384/resolve/main/sigclip_vision_patch14_384.safetensors
