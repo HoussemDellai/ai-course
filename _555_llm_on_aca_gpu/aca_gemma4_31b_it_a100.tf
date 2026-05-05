33a42e67de3fdb4bade58996b860b6f1e37700b1
Houssem Dellai <houssem.dellai@live.com>
2026-05-05 21:54:03 +0200

    work in progress

--- _555_llm_on_aca_gpu/aca_gemma4_31b_it_a100.tf
+++ _555_llm_on_aca_gpu/aca_gemma4_31b_it_a100.tf
@@ -39,6 +39,7 @@ resource "azurerm_container_app" "aca_gemma4_31b_it_a100" {
         "--max-model-len", "262144",
         "--gpu-memory-utilization", "0.85",
         "--limit-mm-per-prompt", jsonencode({ "images" : 4, "videos" : 1, "audios" : 1 }),
+        "--speculative-config", jsonencode({"model": "gg-hf-am/gemma-4-31B-it-assistant", "num_speculative_tokens": 4}),
         "--enable-auto-tool-choice",
         "--tool-call-parser", "gemma4",
         "--reasoning-parser", "gemma4",
@@ -46,6 +47,19 @@ resource "azurerm_container_app" "aca_gemma4_31b_it_a100" {
         "--host", "0.0.0.0",
         "--port", "8000"
       ]
+      # args = [
+      #   "--model", "google/gemma-4-31B-it",
+      #   "--tensor-parallel-size", "1",
+      #   "--max-model-len", "262144",
+      #   "--gpu-memory-utilization", "0.85",
+      #   "--limit-mm-per-prompt", jsonencode({ "images" : 4, "videos" : 1, "audios" : 1 }),
+      #   "--enable-auto-tool-choice",
+      #   "--tool-call-parser", "gemma4",
+      #   "--reasoning-parser", "gemma4",
+      #   "--chat-template", "examples/tool_chat_template_gemma4.jinja",
+      #   "--host", "0.0.0.0",
+      #   "--port", "8000"
+      # ]
 
       # # Optional: HF token if needed for gated models
       # env {
