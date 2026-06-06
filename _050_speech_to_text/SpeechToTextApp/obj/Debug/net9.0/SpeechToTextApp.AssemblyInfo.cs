148f46ff36a93b55b168f26fe33159d807dd6f59
Houssem Dellai <houssem.dellai@live.com>
2026-06-06 20:49:54 +0200

    work in progress

--- _050_speech_to_text/SpeechToTextApp/obj/Debug/net9.0/SpeechToTextApp.AssemblyInfo.cs
+++ _050_speech_to_text/SpeechToTextApp/obj/Debug/net9.0/SpeechToTextApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpeechToTextApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8da9c3757cc4e8cb08bcab00c694434f1e338a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cccee4d2d7b4e5f1b69f858896cbcad12d6c32dc")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpeechToTextApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpeechToTextApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
