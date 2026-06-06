0cb64b5496ae38d0881df980f08473781e5f02f6
Houssem Dellai <houssem.dellai@live.com>
2026-06-06 15:58:48 +0200

    work in progress

--- 020_azure_openai_csharp/obj/Debug/net8.0/200_azure_openai_csharp.AssemblyInfo.cs
+++ 020_azure_openai_csharp/obj/Debug/net8.0/200_azure_openai_csharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("200_azure_openai_csharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81ec14d95db6c308726ac2417bfe3775055e4580")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8da9c3757cc4e8cb08bcab00c694434f1e338a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("200_azure_openai_csharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("200_azure_openai_csharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- _050_speech_to_text/SpeechToTextApp/obj/Debug/net9.0/SpeechToTextApp.AssemblyInfo.cs
+++ _050_speech_to_text/SpeechToTextApp/obj/Debug/net9.0/SpeechToTextApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpeechToTextApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+81ec14d95db6c308726ac2417bfe3775055e4580")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8da9c3757cc4e8cb08bcab00c694434f1e338a1")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpeechToTextApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpeechToTextApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
